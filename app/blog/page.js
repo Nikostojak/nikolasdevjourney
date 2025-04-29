bc0bd4ac32f4fa3a851b563a0493cc2f6604f034
Nikostojak <stojaknikolas@gmail.com>
2025-04-29 14:07:39 +0200

    Fix import paths to match file names

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,7 +1,8 @@
 'use client';
 
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
+import Navbar from '../components/navbar'
+import Footer from '../components/footer'
+
 
 export default function BlogPage() {
   return (
--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
 
 export default function ContactPage() {
   return (
