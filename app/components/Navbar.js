b07d37d643a541a3050e75c33a21cacb607e1a46
Nikostojak <stojaknikolas@gmail.com>
2025-04-29 16:44:55 +0200

    Fix navigation links with Next.js Link

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
     <nav
@@ -25,10 +27,10 @@ export default function Navbar() {
           flexWrap: 'wrap',
         }}
       >
-        <a href="#" style={{ color: 'white', textDecoration: 'none' }}>Home</a>
-        <a href="#" style={{ color: 'white', textDecoration: 'none' }}>Blog</a>
-        <a href="#" style={{ color: 'white', textDecoration: 'none' }}>About</a>
-        <a href="#" style={{ color: 'white', textDecoration: 'none' }}>Contact</a>
+        <Link href="/" style={{ color: 'white', textDecoration: 'none' }}>Home</Link>
+        <Link href="/blog" style={{ color: 'white', textDecoration: 'none' }}>Blog</Link>
+        <Link href="/about" style={{ color: 'white', textDecoration: 'none' }}>About</Link>
+        <Link href="/contact" style={{ color: 'white', textDecoration: 'none' }}>Contact</Link>
       </div>
     </nav>
   );
