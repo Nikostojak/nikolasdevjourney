57476930803a89fad03af67041efa3851f4dc40e
Nikostojak <stojaknikolas@gmail.com>
2025-04-29 14:14:46 +0200

    Fix import paths to match file names

--- app/page.js
+++ app/page.js
@@ -1,8 +1,7 @@
 'use client';
 
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
-
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 
 export default function HomePage() {
