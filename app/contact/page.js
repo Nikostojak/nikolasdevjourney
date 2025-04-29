37406e0ffc6131272dc1f08770d440028909e123
Nikostojak <stojaknikolas@gmail.com>
2025-04-29 15:42:56 +0200

    Fix import paths to match new components folder location

--- app/contact/page.js
+++ app/contact/page.js
@@ -1,7 +1,8 @@
 'use client';
 
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 
 export default function ContactPage() {
   return (
