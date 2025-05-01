fffdb8dbe285c0101301616d35e124f23ab2b542
Nikostojak <stojaknikolas@gmail.com>
2025-05-01 15:41:39 +0200

    Fix incorrect import paths for Navbar and Footer

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Navbar from '../../components/Navbar';
-import Footer from '../../components/Footer';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function BlogPage() {
   const [posts, setPosts] = useState([]);
