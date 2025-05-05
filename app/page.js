2461f30f4f2c721623e6a3e2e7435aa32f0b6d4f
Nikostojak <stojaknikolas@gmail.com>
2025-05-05 18:59:26 +0200

    Fix incorrect import paths for Navbar and Footer

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client';
 
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
-import Container from '../components/Container';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Container from './components/Container';
 import Link from 'next/link';
 
 export default function HomePage() {
